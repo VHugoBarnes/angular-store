afc49c26035fdbd2be59bcb5d23913c2bd91a535
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-13 19:24:39 -0600

    code split and prefetching

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from "./domains/products/pages/list/list.component";
-import { AboutComponent } from "./domains/info/pages/about/about.component";
-import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
-import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 
 
 export const routes: Routes = [
@@ -13,20 +9,20 @@ export const routes: Routes = [
     children: [
       {
         path: "",
-        component: ListComponent
+        loadComponent: () => import("./domains/products/pages/list/list.component").then(c => c.ListComponent)
       },
       {
         path: "product/:id",
-        component: ProductDetailComponent
+        loadComponent: () => import("./domains/products/pages/product-detail/product-detail.component").then(c => c.ProductDetailComponent)
       },
       {
         path: "about",
-        component: AboutComponent
+        loadComponent: () => import("./domains/info/pages/about/about.component").then(c => c.AboutComponent)
       },
     ]
   },
   {
     path: "**",
-    component: NotFoundComponent
+    loadComponent: () => import("./domains/shared/components/layout/layout.component").then(c => c.LayoutComponent)
   }
 ];
