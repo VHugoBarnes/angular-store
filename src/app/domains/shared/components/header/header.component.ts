78c72a8f15c80fefb576ed8e4b400aac69cca9be
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-13 18:17:46 -0600

    Using RouterLinkActive

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CartService } from '@shared/services/cart.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
